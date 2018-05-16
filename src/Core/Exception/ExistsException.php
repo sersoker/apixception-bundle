412364e3b1e2f30a8f8a330153354594114c1096
Juan Gabriel Rodríguez Carrión <jgrodriguezcarrion@gmail.com>
2018-05-16 10:26:55 +0200

    Update ExistsException.php

--- src/Core/Exception/ExistsException.php
+++ src/Core/Exception/ExistsException.php
@@ -1,6 +1,16 @@
 <?php
 namespace Pccomponentes\Apixception\Core\Exception;
 
-abstract class ExistsException extends NotFoundException
+abstract class ExistsException extends \Exception implements SerializableException
 {
+    public abstract function id(): string;
+    public abstract function resource(): string;
+  
+    public function serialice(): array
+    {
+        return  [
+            'id' => $this->id(),
+            'resource' => $this->resource()
+        ];
+    }
 }
