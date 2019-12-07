e77cb058e9288de204ff091fcc4c4c71f39e511a
calmohallag <calmohallag@gmail.com>
2019-12-07 10:17:24 +0100

    fixed: symfony tree builder deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,9 +14,9 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('apixception');
         $treeBuilder
-            ->root('apixception')
+            ->getRootNode()
             ->arrayPrototype()
                 ->addDefaultsIfNotSet()
                 ->children()
